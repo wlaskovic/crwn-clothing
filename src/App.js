4467e79f6260bae6aef70753623cbe35bcc1a1d3
wlaskovic <wlaskovic@gmail.com>
2020-08-17 15:23:11 +0200

    updated with header and logo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
