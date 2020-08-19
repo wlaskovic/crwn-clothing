68b26c07cc6f6048cad567d076d67808ed9d9f97
wlaskovic <wlaskovic@gmail.com>
2020-08-19 13:53:37 +0200

    added redux, and ipmlemented userReducer and userActions

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+// this is needed bcs on first load Redux doesn't know the value of state
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
