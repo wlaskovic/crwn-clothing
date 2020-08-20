1ebf4efb143b3526f1f7a8b8cbd24aba3d9fdf16
wlaskovic <wlaskovic@gmail.com>
2020-08-20 15:58:07 +0200

    addig the quantity arrows and corresponding things to them

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -29,7 +29,8 @@ const CheckoutPage = ({ cartItems, total }) => (
         </div>
         {
             cartItems.map(cartItem =>
-                <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            )
         }
         <div className='total'>
             <span>TOTAL ${total}</span>
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,4 +12,14 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+    return cartItems.map(
+        cartItem => 
+        cartItem.id === cartItemToRemove.id ?
+        { ...cartItem, quantity: cartItem.quantity > 1 ? cartItem.quantity - 1 : cartItem.quantity }
+        : cartItem
+    )
 }
