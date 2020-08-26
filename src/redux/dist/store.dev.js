63ac77df05e88c2a2cbbf5161cd4b70d0fc50b54
wlaskovic <wlaskovic@gmail.com>
2020-08-26 16:39:36 +0200

    removing logger from production

---
+++ src/redux/dist/store.dev.js
@@ -0,0 +1,32 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = exports.persistor = exports.store = void 0;
+
+var _redux = require("redux");
+
+var _reduxPersist = require("redux-persist");
+
+var _reduxLogger = _interopRequireDefault(require("redux-logger"));
+
+var _rootReducer = _interopRequireDefault(require("./root-reducer"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(_reduxLogger["default"]);
+}
+
+var store = (0, _redux.createStore)(_rootReducer["default"], _redux.applyMiddleware.apply(void 0, middlewares));
+exports.store = store;
+var persistor = (0, _reduxPersist.persistStore)(store);
+exports.persistor = persistor;
+var _default = {
+  store: store,
+  persistor: persistor
+};
+exports["default"] = _default;
