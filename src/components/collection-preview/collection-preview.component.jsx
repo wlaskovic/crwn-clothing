f064d40897aab6c546a2e17299115f3c3067328e
wlaskovic <wlaskovic@gmail.com>
2020-08-26 12:48:02 +0200

    moved our directory and shop data into its own reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,17 +4,15 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {
-                items
+            {items
                 .filter((item, idx) => idx < 4)
                 .map((item) => (
                     <CollectionItem key={item.id} item={item} />
-                ))
-            }
+                ))}
         </div>
     </div>
 );
--- src/redux/shop/dist/shop.selectors.dev.js
+++ src/redux/shop/dist/shop.selectors.dev.js
@@ -3,9 +3,21 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.selectCollections = void 0;
+exports.selectCollection = exports.selectCollectionsForPreview = exports.selectCollections = void 0;
 
 var _reselect = require("reselect");
+
+var _lodash = _interopRequireDefault(require("lodash.memoize"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
 
 var selectShop = function selectShop(state) {
   return state.shop;
@@ -15,3 +27,19 @@ var selectCollections = (0, _reselect.createSelector)([selectShop], function (sh
   return shop.collections;
 });
 exports.selectCollections = selectCollections;
+var selectCollectionsForPreview = (0, _reselect.createSelector)([selectCollections], function (collections) {
+  return Object.keys(collections).map(function (key) {
+    return collections[key];
+  });
+});
+exports.selectCollectionsForPreview = selectCollectionsForPreview;
+
+var selectCollection = function selectCollection(collectionUrlParam) {
+  return (0, _reselect.createSelector)([selectCollections], function (collections) {
+    return collections.find(function (collection) {
+      return collection.id === COLLECTION_ID_MAP[collectionUrlParam];
+    });
+  });
+};
+
+exports.selectCollection = selectCollection;
